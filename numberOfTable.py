05c9df5fbc0a65a8051b14ae8fed9558682e4339
lab1 <lab1@pc22a.ssuet.edu.pk>
2024-05-27 11:08:09 +0500

    first commit

---
+++ numberOfTable.py
@@ -0,0 +1,6 @@
+#Create a program to generate the table of any number
+
+number = int(input('Enter any number to generate its table.'))
+
+for i in range (1, 11):
+    print(number,'*',i,'=',number * i)
