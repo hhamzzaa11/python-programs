2f0f3bd7460dc759da6e78468582b0515aaf1586
lab1 <lab1@pc22a.ssuet.edu.pk>
2024-05-27 11:27:28 +0500

    first commit

---
+++ biodata.py
@@ -0,0 +1,18 @@
+firstName = str(input("enter your first name"))
+middleName = str(input("enter your middle name"))
+lastName = str(input("anter your last name"))
+age = int(input("enter your age"))
+uni = str(input("enter your uni"))
+field = str(input("enter your field"))
+rollno = int(input("enter your roll"))
+
+print(f"your first name is {firstName}")
+print(f"your middle name is {middleName}")
+print(f"your last name is {lastName}")
+print(f"your age is {age}")
+print(f"your uni is {uni}")
+print(f"your field is {field}")
+print(f"your rollno is {rollno}")
+
+
+
--- numberOfTable.py
+++ numberOfTable.py
@@ -4,3 +4,4 @@ number = int(input('Enter any number to generate its table.'))
 
 for i in range (1, 11):
     print(number,'*',i,'=',number * i)
+
